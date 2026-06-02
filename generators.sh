41d8ea514fb59b115674bfee89ff7f9fa05ed6c8
Predrag Buncic <pbuncic@yahoo.com>
2026-06-02 23:41:28 +0200

    Spliting LCG into generators,externals and key4hep

---
+++ generators.sh
@@ -0,0 +1,75 @@
+package: generators
+description: Physics event generators meta-package (and their generator-only deps)
+version: "1"
+license: GPL-3.0-or-later
+requires:
+  - lcg.bits
+  - CMake
+  - SFGen
+  - apfel
+  - contur
+  - sherpa
+  - sherpa-openmpi
+  - herwig3
+  - rivet
+  - mcutils
+  - mcfm
+  - madgraph5amc
+  - powheg-box-v2
+  - feynhiggs
+  - vbfnlo
+  - FORM
+  - njet
+  - qgraf
+  - gosam_contrib
+  - gosam
+  - thepeg
+  - evtgen
+  - hijing
+  - starlight
+  - qd
+  - hepmcanalysis
+  - mctester
+  - herwig
+  - crmc
+  - hydjet
+  - tauola
+  - hydjetcpp
+  - alpgen
+  - pyquen
+  - baurmc
+  - professor
+  - jhu
+  - superchic
+  - hoppet
+  - hto4l
+  - prophecy4f
+  - ampt
+  - thep8i
+  - epos4
+  - geneva
+  - recola_SM_ATGC_WARSAW
+  - RELAX
+  - unigen
+  - GENIE
+  - babayaga
+  - bhlumi
+  - cepgen
+  - fastnlo_toolkit
+  - guinea_pig
+  - lhapdfsets
+  - rapidsim
+  - whizard
+  - dpmjet
+# - compilebox
+  - hepmc
+  - heputils
+  - photos
+  - pyslha
+  - pythia6
+  - tauolacpp
+  - yoda
+build_requires:
+  - bits-recipe-tools
+  - "GCC-Toolchain:(?!osx)"
+---
