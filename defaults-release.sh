78e7de3cc6028923db81d67725ad1edec35544c0
Predrag Buncic <pbuncic@yahoo.com>
2026-07-11 16:27:19 +0200

    defaults-release: CVMFS path under system: (prefix + templates, platform-last)

--- defaults-release.sh
+++ defaults-release.sh
@@ -24,6 +24,13 @@ version: v1
 system:
   sandbox_network: "off"
   build_oversubscribe: 1.25
+  # CVMFS path templates (this group's structural choice; never hashed).
+  # Recorded in .meta.json; publish resolves {prefix} + {pkg}/{tag}/{platform}.
+  prefix:                     "/cvmfs/sft-nightlies-test.cern.ch/lcg/releases"
+  cvmfs_user_prefix:          "/cvmfs/sft-nightlies-test.cern.ch/lcg/user"  # sibling of releases, not {prefix}/user
+  cvmfs_path_template:        "{prefix}/{pkg}/{tag}/{platform}"
+  cvmfs_modules_template:     "{prefix}/{platform}/Modules/modulefiles/{pkg}"
+  cvmfs_shared_path_template: "{prefix}/noarch/{pkg}/{tag}"
 
 env:
   CXXFLAGS: "-fPIC -g -O2"
